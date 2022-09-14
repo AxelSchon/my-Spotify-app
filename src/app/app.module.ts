d40029f1bec4b2832540cb95d347db43d79c623b
AxelSchon <ams0190@mix.wvu.edu>
2022-09-13 21:13:14 -0400

    Added lower information bar with  gradient color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './navigation/top-nav-bar/top-nav-bar.component';
 import { SecNavBarComponent } from './navigation/sec-nav-bar/sec-nav-bar.component';
+import { InformationBarComponent } from './navigation/information-bar/information-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopNavBarComponent,
-    SecNavBarComponent
+    SecNavBarComponent,
+    InformationBarComponent
   ],
   imports: [
     BrowserModule,
