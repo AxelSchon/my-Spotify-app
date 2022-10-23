dd0a5a3743df7056a0aea189634a1264384adc44
AxelSchon <ams0190@mix.wvu.edu>
2022-10-23 18:42:32 -0400

    updated ChangeLog

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,3 +14,27 @@ export const environment = {
  * on performance if an error is thrown.
  */
 // import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAj3EiILSsWFoFQNQoAjvOfGKd7lf6Sr6o",
+  authDomain: "my-spotify-app-26c53.firebaseapp.com",
+  databaseURL: "https://my-spotify-app-26c53-default-rtdb.firebaseio.com",
+  projectId: "my-spotify-app-26c53",
+  storageBucket: "my-spotify-app-26c53.appspot.com",
+  messagingSenderId: "813204845692",
+  appId: "1:813204845692:web:de9d8440f01d667328c5f9",
+  measurementId: "G-48QZ265ZZG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
